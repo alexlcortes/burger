e157492fed34337591c6ecf7cd94badc2b894861
Alejandro Cortes <Alejandro Cortes>
2016-07-05 22:45:23 -0400

    added info to mysql

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+use burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Primetime');
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Solly Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Dirty South Burger');
