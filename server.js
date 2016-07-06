cfcb00707a39fd2d0f8e1d67500b0af51db28b16
Alejandro Cortes <Alejandro Cortes>
2016-07-06 16:18:51 -0400

    sever work

--- server.js
+++ server.js
@@ -3,3 +3,26 @@ var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
 var app = express();
+//Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + '/public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Root',
+  database : 'burgers_db'
+});
