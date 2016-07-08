7b588402529d71b5d96a200ce8d2ec03fe24134a
Alejandro Cortes <Alejandro Cortes>
2016-07-08 14:24:13 -0400

    added corrections to server.js

--- server.js
+++ server.js
@@ -1,72 +1,31 @@
+// dependancies
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
 var app = express();
+
+var exphbs = require('express-handlebars');
+var connection =require('./config/connection.js');
+var path = require('path');
+
+var PORT = process.env.PORT || 3000;
 //Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + '/public'));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
+app.use(bodyParser.urlencoded({extended: false}));
 
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'))
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
+app.use(express.static('public/assets'));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
 app.set('view engine', 'handlebars');
 
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'Root',
-  database : 'burgers_db'
-});
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  };
+// port listener
+app.listen(port, function) {
 
-  console.log('connected as id ' + connection.threadId);
+console.log("Please eat a burger. You are on Port: " + PORT);
 
 });
-
-app.get('/', function(req,res) {
-    connection.query('SELECT * FROM plans;', function(err, data) {
-      if (err) throw err;
-
-      res.render('index', {plans: data});
-
-    });
-});
-
-app.post('/create', function(req,res){
-    connection.query('INSERT INTO plans (plan) VALUES (?)', [req.body.plan], function(err, result) {
-      if (err) throw err;
-      res.redirect('/');
-    });
-});
-
-app.delete('/delete', function(req,res){
-    connection.query('DELETE FROM plans WHERE id = ?', [req.body.id], function(err, result) {
-      if (err) throw err;
-      res.redirect('/');
-    });
-});
-
-app.put('/update', function(req,res){
-    connection.query('UPDATE plans SET plan = ? WHERE id = ?', [req.body.plan, req.body.id], function(err, result) {
-      if (err) throw err;
-      res.redirect('/');
-    });
-});
-
-var port = 3000;
-app.listen(port);
-console.log("Please eat a burger");
